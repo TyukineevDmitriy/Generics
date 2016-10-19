0c417d2a525ca3a39527b0eb47f0b7456751b630
dvtgtt <td-msaa@yandex.ru>
2016-10-19 20:44:35 +0500

    1

---
+++ Generics/ProcessorBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class ProcessorBuilder
+    {
+        public static ProcessorBuilder<TEngine> CreateEngine<TEngine>()
+        {
+            return new ProcessorBuilder<TEngine>();
+        }
+    }
+    class ProcessorBuilder<TEngine>
+    {
+        public ProcessorBuilder<TEngine, TEntity> For<TEntity>()
+        {
+            return new ProcessorBuilder<TEngine, TEntity>();
+        }
+    }
+    class ProcessorBuilder<TEngine, TEntity>
+    {
+        public Processor<TEngine, TEntity, TLogger> With<TLogger>()
+        {
+            return new Processor<TEngine, TEntity, TLogger>();
+        }
+    }
+}
